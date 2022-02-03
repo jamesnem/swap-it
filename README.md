368d59cc50081254c63a2cf97d1488cb236490cb
jamesnem <jenemecek@deakin.edu.au>
2022-02-04 01:03:05 +1100

    Login and registration page linked w seller page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-
+Login and Registration pages created by James Nemecek
+Seller page created by Muhammad
+Buyer page created by Bose
+User chat created by Julia
--- public/env.js
+++ public/env.js
@@ -1,14 +1,16 @@
 var passwordValidate = document.getElementById("registerPassword");
 var confirmValidate = document.getElementById("confirmPassword");
 
+//Create function that compares password and confirm password input fields
 function validatePassword() {
-    if (passwordValidate.value != confirmValidate.value) {
+    if (passwordValidate.value != confirmValidate.value ) {
         confirmValidate.setCustomValidity('Passwords don\'t\ match');
     }
     else {
         confirmValidate.setCustomValidity('')
     }
 }
+
 
 $(document).ready(function () {
     passwordValidate.onchange = validatePassword();
