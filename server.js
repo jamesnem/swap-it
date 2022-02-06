3ae39a1c81964f2a4c3134fab30d93ae53b4d6ed
JuliaAxelrod <jaxelrod@deakin.edu.au>
2022-02-07 00:49:32 +1100

    changed port to 8090

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8090;
 
 app.use(express.static(__dirname + '/public'));
 
